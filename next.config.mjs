d06d4e81178a4e2c8039ac21a4b6142485aca270
MortenKirch <morten-kris@live.dk>
2024-12-03 14:54:02 +0100

    fix?

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Enables static export
-    basePath: '/repo-name', // Replace 'repo-name' with your GitHub repository name if deploying to a subpath
+    basePath: '/advanced-frontend', // Replace 'repo-name' with your GitHub repository name if deploying to a subpath
     trailingSlash: true, // Ensures all routes have a trailing slash for compatibility
   };
   
