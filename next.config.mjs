de5be13354ae8027ddf0338e342eff69b0c6791a
MortenKirch <morten-kris@live.dk>
2024-12-03 15:14:05 +0100

    removed basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Enables static export
-    basePath: '/advanced-frontend', // Replace 'repo-name' with your GitHub repository name if deploying to a subpath
+
   };
   
   export default nextConfig;
