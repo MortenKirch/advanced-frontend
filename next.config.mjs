3bc0cb3ecd097db97fa0ba5f47666bbe1e1d61b1
MortenKirch <morten-kris@live.dk>
2024-12-03 15:27:06 +0100

    Vercel working

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static export
-  basePath: '/advanced-frontend', // Replace <repository-name> with your actual repo name
-  assetPrefix: '/advanced-frontend',
 };
   
   export default nextConfig;
