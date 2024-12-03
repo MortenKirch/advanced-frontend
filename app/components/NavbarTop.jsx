a1703aa217949cc70b65bd2001295d2e542a52a5
MortenKirch <morten-kris@live.dk>
2024-12-03 14:26:16 +0100

    Update NavbarTop.jsx

--- app/components/NavbarTop.jsx
+++ app/components/NavbarTop.jsx
@@ -1,20 +1,21 @@
 import { FaBell, FaSignInAlt } from 'react-icons/fa'; // Import icons from react-icons
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function NavbarTop() {
     return (
         <header className="fixed top-0 w-full bg-creme text-f-green">
             <nav className="container mx-auto px-4 py-2 flex justify-between items-center align-center h-16">
-            <a href="/">
+            <Link href="/">
                 <Image src="/gulerod.png" alt="Logo" width={100} height={30} />
-                </a>
+                </Link>
                 <div className="flex flex-row w-30">
-                    <a href="/notification">
+                    <Link href="/notification">
                     <FaBell title="Notifications" aria-label="Notifications"  size={25}/>
-                    </a>
-                    <a href="/login">
+                    </Link>
+                    <Link href="/login">
                     <FaSignInAlt title="Login" aria-label="Login" className="ml-6" size={25}/>
-                    </a>
+                    </Link>
                 </div>
             </nav>
         </header>
