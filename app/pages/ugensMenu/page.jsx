662d619bed99fb1c5734ece22b64d5730cc3a92d
MortenKirch <morten-kris@live.dk>
2024-12-04 15:17:52 +0100

    update layout ugens menu

--- app/pages/ugensMenu/page.jsx
+++ app/pages/ugensMenu/page.jsx
@@ -85,6 +85,49 @@ export default function UgensMenu() {
   </div>
 
 
+    
+  <div className="col-span-2">
+    <div className="flex justify-between  ">
+    <h2>Varm ret</h2>
+    <button className="underline text-xs self-end mr-2">se vegetar muligheder her</button>
+    </div>
+      <div className=" bg-tan overflow-hidden rounded-large">
+     
+      
+      <div className="flex w-full justify-between">
+        <h3 className="pl-2 font-bold">undertekst omkring den ret som skal være her</h3>
+        <RiInformation2Line size={30} className="m-1 mr-2"/>
+        </div>
+        <Image src="/bread.png" width={150} height={150} alt="billede af grøntsager"className="self-end"/>
+  
+        </div>
+      
+  </div>
+
+
+  <div className="grid grid-cols-2 col-span-2 gap-3">
+
+<h2 className="col-span-2 text-left">Pålæg</h2>
+
+
+  <div className="flex bg-primary rounded-large">
+
+    <div>
+    <h3 className="p-2 font-bold">salat omkring den ret som skal være her</h3>
+    <Image src="/cold-cut2.png" width={250} height={250} alt="billede af grøntsager"className="w-full"/>
+    </div>
+    <RiInformation2Line size={45} className="m-1 mr-2 "/>
+  </div>
+  <div className="flex bg-primary rounded-large">
+
+  <div className="col-span-2 flex flex-col">
+  <h3 className="p-2 font-bold">salat omkring den ret som skal være her</h3>
+  <Image src="/cold-cut.png" width={150} height={150} alt="billede af grøntsager"className="self-end"/>
+  </div>
+  <RiInformation2Line size={45} className="m-1 mr-2 "/>
+  </div>
+</div>
+
 </section>
 
 
