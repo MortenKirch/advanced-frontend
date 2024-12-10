2d8b5c63d0fbfb9522f74f8feaf55c4edc0a4e36
MortenKirch <morten-kris@live.dk>
2024-12-10 12:49:43 +0100

    removed import

--- app/pages/layout.js
+++ app/pages/layout.js
@@ -2,7 +2,6 @@ import Navbar from "../components/Navbar";
 import NavbarTop from "../components/NavbarTop";
 import BackButton from "../components/BackButton";
 import "../globals.css"
-import "../codetest.js"
 export default function pageLayout({ children }) {
   return (
 
