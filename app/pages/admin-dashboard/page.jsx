57bba96b994152ae463acdb58210e5f5cb0b8d86
MortenKirch <morten-kris@live.dk>
2024-12-16 15:03:16 +0100

    admin page width fixed

--- app/pages/admin-dashboard/page.jsx
+++ app/pages/admin-dashboard/page.jsx
@@ -4,13 +4,13 @@ import { FaEdit } from "react-icons/fa";
 export default function AdminDashboard() {
   return (
     <>
-      <section className="m-4 w-full flex flex-col items-center">
+      <section className=" m-4 flex flex-col items-center">
         <h1 className="font-sans text-3xl font-bold mb-4">Admin Dashboard</h1>
         <h3 className="text-center text-lg text-gray-600 mb-8">
           Her kan du administrere og redigere indholdet
         </h3>
 
-        <div className="grid grid-cols-1 gap-6 w-full max-w-md">
+        <div className="grid grid-cols-1 gap-6 w-full">
           <Link
             href="admin-dashboard/edit-menu"
             className="flex items-center justify-between p-4 w-full bg-forest text-white rounded-lg shadow-lg hover:bg-green-700 transition duration-300"
